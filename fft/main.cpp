a7ef0ae69d4932eeb8361ca764b6a05d47dfb8ac
swift_file <sky.y.m.318@gmail.com>
2022-09-15 14:47:49 +0900

    hotfix,chore

--- fft/main.cpp
+++ fft/main.cpp
@@ -27,57 +27,61 @@ int main() {
     for (int i = 0; i < horizontal_cut_num; i++) {
         edge_points.at(i).y = i * cut_height;
     }
-    std::vector<cv::Mat> rois;
-    for (int i = 0; i < horizontal_cut_num; ++i) {
-        for (int j = 0; j < vertial_cut_num; ++j) {
-            cv::Mat var_roi;
-            var_roi = orig_image(cv::Rect(j * cut_width, i * cut_height, cut_width, cut_height));
-            rois.emplace_back(var_roi);
-        }
-    }
-    std::cout << "cut image num is " << rois.size() << std::endl;
+//    std::vector<cv::Mat> rois;
+//    for (int i = 0; i < horizontal_cut_num; ++i) {
+//        for (int j = 0; j < vertial_cut_num; ++j) {
+//            cv::Mat var_roi;
+//            var_roi = orig_image(cv::Rect(j * cut_width, i * cut_height, cut_width, cut_height));
+//            rois.emplace_back(var_roi);
+//        }
+//    }
+//    std::cout << "cut image num is " << rois.size() << std::endl;
     auto start_time = std::chrono::system_clock::now();
     //calc fft
-    for (int ite = 0; ite < rois.size(); ite++) {
-        Mat var_img = rois.at(ite);
-        Mat padded;
-        int m = getOptimalDFTSize(var_img.rows);
-        int n = getOptimalDFTSize(var_img.cols);
-        copyMakeBorder(var_img, padded, 0, m - var_img.rows, 0, n - var_img.cols, BORDER_CONSTANT,
-                       Scalar::all(0));
-        Mat planes[] = {Mat_<float>(padded), Mat::zeros(padded.size(), CV_32F)};
-        Mat complexI;
-        merge(planes, 2, complexI);
-        dft(complexI, complexI);
-        split(complexI, planes);
-        magnitude(planes[0], planes[1], planes[0]);
-        Mat magI = planes[0];
-        magI += Scalar::all(1);
-        log(magI, magI);
-        magI = magI(Rect(0, 0, magI.cols & -2, magI.rows & -2));
-        int cx = magI.cols / 2;
-        int cy = magI.rows / 2;
-        Mat q0(magI, Rect(0, 0, cx, cy));
-        Mat q1(magI, Rect(cx, 0, cx, cy));
-        Mat q2(magI, Rect(cx, cy, cx, cy));
-        Mat q3(magI, Rect(cx, cy, cx, cy));
-        Mat tmp;
-        q0.copyTo(tmp);
-        q3.copyTo(q0);
-        tmp.copyTo(q3);
-        q1.copyTo(tmp);
-        q2.copyTo(q1);
-        tmp.copyTo(q2);
-        normalize(magI, magI, 0, 1, NORM_MINMAX);
-//        imshow("Input", var_img);
-//        imshow("spectrum magnitude", magI);
-        Mat result_img = magI.clone();
-        result_img.convertTo(result_img, CV_8UC1, 255);
-        std::ostringstream oss;
-        oss << std::setfill('0') << std::setw(3) << ite;
-        cv::imwrite("../outputs/fft/result_roi_" + oss.str() + ".png", result_img);
-        std::cout << "image is saved!" << std::endl;
-    }
+    //    for (int ite = 0; ite < rois.size(); ite++) {
+    Mat var_img = orig_image.clone();
+    Mat padded;
+    int m = getOptimalDFTSize(var_img.rows);
+    int n = getOptimalDFTSize(var_img.cols);
+    copyMakeBorder(var_img, padded, 0, m - var_img.rows, 0, n - var_img.cols, BORDER_CONSTANT,
+                   Scalar::all(0));
+    Mat planes[] = {Mat_<float>(padded), Mat::zeros(padded.size(), CV_32F)};
+    Mat complexI;
+    merge(planes, 2, complexI);
+    dft(complexI, complexI);
+    split(complexI, planes);
+    magnitude(planes[0], planes[1], planes[0]);
+    Mat magI = planes[0];
+    magI += Scalar::all(1);
+    log(magI, magI);
+//    magI = magI(Rect(0, 0, magI.cols & -2, magI.rows & -2));
+    int cx = magI.cols / 2;
+    int cy = magI.rows / 2;
+    Mat q0(magI, Rect(0, 0, cx, cy));
+    Mat q1(magI, Rect(cx, 0, cx, cy));
+    Mat q2(magI, Rect(0, cy, cx, cy));
+    Mat q3(magI, Rect(cx, cy, cx, cy));
+    Mat tmp;
+    q0.copyTo(tmp);
+    q3.copyTo(q0);
+    tmp.copyTo(q3);
+    q1.copyTo(tmp);
+    q2.copyTo(q1);
+    tmp.copyTo(q2);
+    normalize(magI, magI, 0, 1, NORM_MINMAX);
+    resize(var_img, var_img, cv::Size(), 3, 3);
+    resize(magI, magI, cv::Size(), 3, 3);
+    imshow("Input", var_img);
+    imshow("spectrum magnitude", magI);
+    cv::waitKey();
+    Mat result_img = magI.clone();
+    result_img.convertTo(result_img, CV_8UC1, 255);
+    std::ostringstream oss;
+//        oss << std::setfill('0') << std::setw(3) << ite;
+    cv::imwrite("../outputs/fft/result.png", result_img);
+//        cv::imwrite("../outputs/fft/result_roi_" + oss.str() + ".png", result_img);
+    std::cout << "image is saved!" << std::endl;
+//    }
     auto end_time = std::chrono::system_clock::now();
     double elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
     std::cout << "elapsed time is " << elapsed_time << "[ms]" << std::endl;
